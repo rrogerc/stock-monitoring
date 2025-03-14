654be3e776cee1e5e5c142f68d1d3d78fe1c7002
Roger Chen <rogerchen803@gmail.com>
2025-03-14 10:40:14 -0600

    sprint initializr

---
+++ src/test/java/com/rrogerc/stock_monitoring/StockMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrogerc.stock_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
