145fa40236b9e7868df08a6619ac444e93b134f1
Roger Chen <rogerchen803@gmail.com>
2025-03-15 08:36:07 -0600

    add aws msk and iam authentication

--- src/main/java/com/rrogerc/stock_monitor/config/KafkaConsumerConfig.java
+++ src/main/java/com/rrogerc/stock_monitor/config/KafkaConsumerConfig.java
@@ -4,7 +4,6 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -12,17 +11,31 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
-@EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "stock-group");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "b-2-public.stockmonitorcluster.o82tll.c2.kafka.ca-central-1.amazonaws.com:9198," +
+                        "b-3-public.stockmonitorcluster.o82tll.c2.kafka.ca-central-1.amazonaws.com:9198," +
+                        "b-1-public.stockmonitorcluster.o82tll.c2.kafka.ca-central-1.amazonaws.com:9198");
+
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        // AWS MSK IAM Authentication
+        config.put("security.protocol", "SASL_SSL");
+        config.put("sasl.mechanism", "AWS_MSK_IAM");
+        config.put("sasl.jaas.config", "software.amazon.msk.auth.iam.IAMLoginModule required;");
+        config.put("sasl.client.callback.handler.class", "software.amazon.msk.auth.iam.IAMClientCallbackHandler");
+        config.put("client.dns.lookup", "use_all_dns_ips");
+
+        // Use AWS environment variables or default profile
+        config.put("aws.access.key.id", System.getenv("AWS_ACCESS_KEY_ID"));
+        config.put("aws.secret.key.id", System.getenv("AWS_SECRET_ACCESS_KEY"));
+        config.put("aws.region", System.getenv("AWS_REGION"));
 
         return new DefaultKafkaConsumerFactory<>(config);
     }
--- src/main/java/com/rrogerc/stock_monitor/config/KafkaProducerConfig.java
+++ src/main/java/com/rrogerc/stock_monitor/config/KafkaProducerConfig.java
@@ -17,15 +17,23 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "b-1.stock-monitor-cluster.xyz.kafka.us-east-1.amazonaws.com:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "b-2-public.stockmonitorcluster.o82tll.c2.kafka.ca-central-1.amazonaws.com:9198," +
+                        "b-3-public.stockmonitorcluster.o82tll.c2.kafka.ca-central-1.amazonaws.com:9198," +
+                        "b-1-public.stockmonitorcluster.o82tll.c2.kafka.ca-central-1.amazonaws.com:9198");
+
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
-        // AWS-specific authentication settings
         config.put("security.protocol", "SASL_SSL");
         config.put("sasl.mechanism", "AWS_MSK_IAM");
         config.put("sasl.jaas.config", "software.amazon.msk.auth.iam.IAMLoginModule required;");
         config.put("sasl.client.callback.handler.class", "software.amazon.msk.auth.iam.IAMClientCallbackHandler");
+
+        // Use AWS environment variables or default profile
+        config.put("aws.access.key.id", System.getenv("AWS_ACCESS_KEY_ID"));
+        config.put("aws.secret.key.id", System.getenv("AWS_SECRET_ACCESS_KEY"));
+        config.put("aws.region", System.getenv("AWS_REGION"));
 
         return new DefaultKafkaProducerFactory<>(config);
     }
@@ -35,3 +43,5 @@ public class KafkaProducerConfig {
         return new KafkaTemplate<>(producerFactory());
     }
 }
+
+
