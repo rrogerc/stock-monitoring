596ab275853c4b21b1d471239844532dd1c924aa
Roger Chen <rogerchen803@gmail.com>
2025-03-14 19:54:41 -0600

    create aws msk cluster and update kafka produce config

--- src/main/java/com/rrogerc/stock_monitor/config/KafkaProducerConfig.java
+++ src/main/java/com/rrogerc/stock_monitor/config/KafkaProducerConfig.java
@@ -17,9 +17,16 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "b-1.stock-monitor-cluster.xyz.kafka.us-east-1.amazonaws.com:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        // AWS-specific authentication settings
+        config.put("security.protocol", "SASL_SSL");
+        config.put("sasl.mechanism", "AWS_MSK_IAM");
+        config.put("sasl.jaas.config", "software.amazon.msk.auth.iam.IAMLoginModule required;");
+        config.put("sasl.client.callback.handler.class", "software.amazon.msk.auth.iam.IAMClientCallbackHandler");
+
         return new DefaultKafkaProducerFactory<>(config);
     }
 
